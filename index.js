af52ee428e1d204eaed910358fdd28ce4a76ab39
Blake Runyon <blakerunyon09@gmail.com>
2021-05-10 22:23:33 -0600

    Heroku SSL Settings 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 var cors = require('cors')
 require('dotenv').config()
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 
 // JSON MIDDLEWARE
 app.use(express.json())
