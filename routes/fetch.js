b31549549a0e8462a35f7d2b801be19dcf39da67
Blake Runyon <blakerunyon09@gmail.com>
2021-05-10 20:57:31 -0600

    Added JWT Recovery

--- routes/fetch.js
+++ routes/fetch.js
@@ -2,28 +2,17 @@ const router = require('express').Router()
 const axios = require('axios').default;
 
 router.get('/', (req, res) => {
+  const body = JSON.parse(req.headers.body)
 
-  // res.send({
-  //   headers: req.headers.headers, 
-  //   body: req.headers.body,
-  //   endpoint: req.headers.endpoint
-  // })
-
-  axios.post(req.headers.endpoint, {
-    appToken:"Px7U0We8xt9MKovt8kzYioy2KyfGvbv9Expp4GMagwUBcPVpvoI04nKxTSnC+A8j",
-    request:{
-      applicationName:"Avid4AdventureNew",
-      userName:"blake@avid4.com",
-      password:"Gigglys5",
-      seasonIds:[]
-    }
-  },{
+  axios.post(req.headers.endpoint,
+    JSON.parse(req.headers.body)
+  ,{
     headers: {
       ...req.headers.headers
     }
   })
   .then(function (response) {
-    res.send({res: response.data});
+    res.send(response.data);
   })
   .catch(function (error) {
     res.send({error: error.data});
